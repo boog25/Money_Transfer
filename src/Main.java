04291fc3b4e8fc953f1d72cd374b99df932b3ed3
boog25 <voroshilov.d.v123@gmail.com>
2020-12-18 20:26:48 +0500

    report

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class  Main {
+    public static void main (String[] bank) {
+        int score = 2_000_000_000;
+        int transfer = 500_000_000;
+        int total = score + transfer;
+        System.out.println(total);
+    }
+
+}
